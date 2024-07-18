71ad9a813badf227aa762ff71cff790576e92dbd
Alexandre Caron <alexandre.caron12@msp.gouv.qc.ca>
2024-07-18 08:54:04 -0400

    fix(demo): basic test for external-links

--- projects/demo/src/app/components/external-link/external-link.component.spec.ts
+++ projects/demo/src/app/components/external-link/external-link.component.spec.ts
@@ -1,9 +1,11 @@
+import { ComponentRef } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExternalLinkComponent } from './external-link.component';
 
 describe('ExternalLinkComponent', () => {
   let component: ExternalLinkComponent;
+  let ref: ComponentRef<ExternalLinkComponent>;
   let fixture: ComponentFixture<ExternalLinkComponent>;
 
   beforeEach(async () => {
@@ -13,6 +15,11 @@ describe('ExternalLinkComponent', () => {
 
     fixture = TestBed.createComponent(ExternalLinkComponent);
     component = fixture.componentInstance;
+    ref = fixture.componentRef;
+
+    ref.setInput('title', 'Test');
+    ref.setInput('link', 'test');
+
     fixture.detectChanges();
   });
 

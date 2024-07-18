2a90e9345ef8c79e47d6891081965f464b50d276
Alexandre Caron <alexandre.caron12@msp.gouv.qc.ca>
2024-07-18 08:48:19 -0400

    fix: linting

--- projects/demo/src/app/components/external-link/external-link.component.spec.ts
+++ projects/demo/src/app/components/external-link/external-link.component.spec.ts
@@ -9,9 +9,8 @@ describe('ExternalLinkComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ExternalLinkComponent]
-    })
-    .compileComponents();
-    
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ExternalLinkComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- projects/demo/src/app/pages/components/showcases/button/button.component.spec.ts
+++ projects/demo/src/app/pages/components/showcases/button/button.component.spec.ts
@@ -9,9 +9,8 @@ describe('ButtonComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ButtonComponent]
-    })
-    .compileComponents();
-    
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();

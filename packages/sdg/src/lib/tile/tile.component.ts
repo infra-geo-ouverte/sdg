7ec1a6e6db0b0581033ebcda7e7a69764e34382d
Maxime Lamer <maxime.lamer@msp.gouv.qc.ca>
2024-08-26 13:18:28 -0400

    feat(tile): title validation and internal routing

--- packages/sdg/src/lib/tile/tile.component.ts
+++ packages/sdg/src/lib/tile/tile.component.ts
@@ -1,8 +1,11 @@
 import { NgFor, NgIf, NgStyle } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 import { IgoLanguageModule } from '@igo2/core/language';
+
+import { environment } from 'projects/demo/src/environments/environment';
 
 @Component({
   selector: 'sdg-tile',
@@ -13,13 +16,31 @@ import { IgoLanguageModule } from '@igo2/core/language';
   styleUrls: ['./tile.component.scss']
 })
 export class TileComponent {
+  constructor(private router: Router) {}
+
   icon = input<string>();
-  title = input.required<string>();
+  title = input.required<string, string>({
+    transform: this.titleValidation
+  });
   message = input<string>();
   href = input.required<string>();
   isHandset = input<boolean>();
 
+  private titleValidation(title: string): string {
+    if (title.length > 45) {
+      if (!environment.production) {
+        console.error('Tile title should not be longer than 45 characters');
+      }
+      return `${title.slice(0, 45)}...`;
+    }
+
+    return title;
+  }
+
   goToLink() {
-    window.open(this.href(), '_blank');
+    const regex: RegExp = /^https?:\/\//;
+    regex.test(this.href())
+      ? window.open(this.href(), '_blank')
+      : this.router.navigate([this.href()]);
   }
 }

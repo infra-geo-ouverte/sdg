894d079d318d06d5a24f0224f88e7244a19c51f0
Maxime Lamer <maxime.lamer@msp.gouv.qc.ca>
2024-09-20 15:32:42 -0400

    refactor(tile): tile adjustments

--- projects/demo/src/app/pages/components/showcases/tile/tile.component.spec.ts
+++ projects/demo/src/app/pages/components/showcases/tile/tile.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { TEST_CONFIG } from 'projects/demo/src/test-config';
+
 import { TileDemoComponent } from './tile.component';
 
 describe('TileDemoComponent', () => {
@@ -8,7 +10,8 @@ describe('TileDemoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TileDemoComponent]
+      imports: [TileDemoComponent],
+      providers: [...TEST_CONFIG.providers!]
     }).compileComponents();
 
     fixture = TestBed.createComponent(TileDemoComponent);

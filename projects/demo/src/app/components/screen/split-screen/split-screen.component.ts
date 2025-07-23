3713800c5b22820ce9e6029e6f7faf80d0726b7e
Maxime Lamer <maxime.lamer@msp.gouv.qc.ca>
2025-07-23 16:26:27 -0400

    fix: added charter banner in demo

--- projects/demo/src/app/components/screen/split-screen/split-screen.component.ts
+++ projects/demo/src/app/components/screen/split-screen/split-screen.component.ts
@@ -1,14 +1,32 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  WritableSignal,
+  input
+} from '@angular/core';
 
-import { BreadcrumbsWithRouterComponent } from '@igo2/sdg-common';
+import {
+  BreadcrumbsWithRouterComponent,
+  CharterBannerComponent
+} from '@igo2/sdg-common';
+import { Language } from '@igo2/sdg-core';
+
+import { AppTranslationService } from '../../../config/translation/translation.service';
 
 @Component({
   selector: 'app-split-screen',
-  imports: [BreadcrumbsWithRouterComponent],
+  imports: [BreadcrumbsWithRouterComponent, CharterBannerComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './split-screen.component.html',
   styleUrl: './split-screen.component.scss'
 })
 export class SplitScreenComponent {
   readonly title = input<string>();
+  readonly showCharterBanner = input<boolean>(false);
+
+  constructor(private translationService: AppTranslationService) {}
+
+  get lang(): WritableSignal<Language> {
+    return this.translationService.lang;
+  }
 }

9d88d8eefed456da09aee6c2ce8a64ec137c7074
Maxime Lamer <83651899+LAMM26@users.noreply.github.com>
2025-08-28 10:26:54 -0400

    fix: removed various button components

--- projects/demo/src/app/pages/components/showcases/button/button.component.ts
+++ projects/demo/src/app/pages/components/showcases/button/button.component.ts
@@ -1,25 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
-import {
-  ButtonComponent,
-  ButtonFlatComponent,
-  ButtonRaisedComponent,
-  ButtonStrokedComponent,
-  ExternalLinkComponent
-} from '@igo2/sdg-common';
+import { ExternalLinkComponent } from '@igo2/sdg-common';
 
 import { ExampleViewerComponent } from 'projects/demo/src/app/components';
 
 @Component({
   selector: 'app-button',
-  imports: [
-    ExampleViewerComponent,
-    ButtonComponent,
-    ButtonStrokedComponent,
-    ButtonRaisedComponent,
-    ButtonFlatComponent,
-    ExternalLinkComponent
-  ],
+  imports: [ExampleViewerComponent, ExternalLinkComponent, MatButtonModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })

c06e4c23df96872fdc93bc282b6fb6c92277a608
Maxime Lamer <maxime.lamer@msp.gouv.qc.ca>
2025-08-29 14:30:38 -0400

    fix: added more tests

--- projects/demo/src/app/pages/components/showcases/checkbox/checkbox.component.spec.ts
+++ projects/demo/src/app/pages/components/showcases/checkbox/checkbox.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { TEST_CONFIG } from 'projects/demo/src/test-config';
 
@@ -21,5 +22,58 @@ describe('CheckboxDemoComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render normal and compact checkboxes', () => {
+    const normalCheckbox = fixture.debugElement.query(
+      By.css('mat-checkbox[value="normal1"]')
+    );
+    const compactCheckbox = fixture.debugElement.query(
+      By.css('mat-checkbox[compact]')
+    );
+    expect(normalCheckbox).toBeTruthy();
+    expect(compactCheckbox).toBeTruthy();
+  });
+
+  it('should have correct class for normal checkbox', () => {
+    const normalCheckbox = fixture.debugElement.query(
+      By.css('mat-checkbox[value="normal1"]')
+    );
+    expect(normalCheckbox.nativeElement.classList).toContain(
+      'mat-mdc-checkbox'
+    );
+  });
+
+  it('should have correct class for compact checkbox', () => {
+    const compactCheckbox = fixture.debugElement.query(
+      By.css('mat-checkbox[compact]')
+    );
+    expect(compactCheckbox.nativeElement.hasAttribute('compact')).toBeTrue();
+  });
+
+  it('should apply custom size to compact checkbox', () => {
+    const compactCheckbox = fixture.debugElement.query(
+      By.css('mat-checkbox[compact]')
+    );
+    const checkboxBackground = compactCheckbox.nativeElement.querySelector(
+      '.mdc-checkbox__background'
+    );
+    const style = window.getComputedStyle(checkboxBackground);
+
+    expect(style.height).toBe('16px');
+    expect(style.width).toBe('16px');
+  });
+
+  it('should apply custom size to normal checkbox', () => {
+    const normalCheckbox = fixture.debugElement.query(
+      By.css('mat-checkbox[value="normal1"]')
+    );
+    const checkboxBackground = normalCheckbox.nativeElement.querySelector(
+      '.mdc-checkbox__background'
+    );
+    const style = window.getComputedStyle(checkboxBackground);
+
+    expect(style.height).toBe('24px');
+    expect(style.width).toBe('24px');
   });
 });

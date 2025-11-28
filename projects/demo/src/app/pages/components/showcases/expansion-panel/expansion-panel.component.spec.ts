2fa3a62855cfaefdf35631074d882badb285ef89
Maxime Lamer <maxime.lamer@msp.gouv.qc.ca>
2025-11-28 10:50:51 -0500

    feat: added expansion-panel component

---
+++ projects/demo/src/app/pages/components/showcases/expansion-panel/expansion-panel.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TEST_CONFIG } from 'projects/demo/src/test-config';
+
+import { ExpansionPanelDemoComponent } from './expansion-panel.component';
+
+describe('ExpansionPanelDemoComponent', () => {
+  let component: ExpansionPanelDemoComponent;
+  let fixture: ComponentFixture<ExpansionPanelDemoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ExpansionPanelDemoComponent],
+      providers: [...TEST_CONFIG.providers!]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ExpansionPanelDemoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

a1261a6db132690a7bea108cc697803a0f0e4740
Alexandre Caron <alexandre.caron12@msp.gouv.qc.ca>
2026-07-17 15:02:44 -0400

    test(carto): scale-line update component name references for consistency

--- packages/carto/ol/src/shared/scale-line/scale-line.component.spec.ts
+++ packages/carto/ol/src/shared/scale-line/scale-line.component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SdgScaleLine } from './scale-line.component';
+import { SdgOlScaleLine } from './scale-line.component';
 
-describe('SdgScaleLine', () => {
-  let component: SdgScaleLine;
-  let fixture: ComponentFixture<SdgScaleLine>;
+describe('SdgOlScaleLine', () => {
+  let component: SdgOlScaleLine;
+  let fixture: ComponentFixture<SdgOlScaleLine>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SdgScaleLine]
+      imports: [SdgOlScaleLine]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(SdgScaleLine);
+    fixture = TestBed.createComponent(SdgOlScaleLine);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });

70cd45c464495282798db5263e0627a523d32c53
Alexandre Caron <alexandre.caron12@msp.gouv.qc.ca>
2026-07-17 06:58:38 -0400

    refactor(scale-line): clean up styles and add onMap input for scale line component

--- packages/carto/ol/src/full-map/scale-line/scale-line.component.ts
+++ packages/carto/ol/src/full-map/scale-line/scale-line.component.ts
@@ -2,6 +2,7 @@ import {
   Component,
   ElementRef,
   afterNextRender,
+  booleanAttribute,
   inject,
   input
 } from '@angular/core';
@@ -13,41 +14,18 @@ import { SdgOlMap } from '../../shared';
 @Component({
   selector: 'sdg-scale-line',
   template: '',
-  styles: `
-    :host::ng-deep .ol-scale-line {
-      background: #fff0;
-      border-radius: 4px;
-
-      position: relative;
-      transform: none !important;
-      left: unset !important;
-      bottom: unset !important;
-      padding: unset !important;
-    }
-
-    :host::ng-deep .ol-scale-line-inner {
-      border: 1px solid
-        var(--mat-sidenav-content-text-color, var(--mat-sys-on-background));
-      border-top: none;
-      color: var(
-        --mat-sidenav-content-text-color,
-        var(--mat-sys-on-background)
-      );
-      font-size: 10px;
-      text-align: center;
-      margin: 1px;
-      will-change: contents, width;
-      transition: all 0.25s;
-
-      text-shadow: unset !important;
-      box-shadow: unset !important;
-    }
-  `
+  styleUrl: './scale-line.component.scss',
+  host: {
+    '[class.sdg-scale-line-on-map]': 'onMap()'
+  }
 })
 export class SdgScaleLine {
   private readonly elementRef = inject<ElementRef>(ElementRef);
 
   readonly map = input.required<SdgOlMap>();
+  readonly onMap = input<boolean, unknown>(false, {
+    transform: booleanAttribute
+  });
 
   constructor() {
     afterNextRender(() => {
